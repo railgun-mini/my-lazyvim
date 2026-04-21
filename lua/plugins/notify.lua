a25aae84ba3c8152d9e326cc1fa0bbf1dd52c086
railgun <1684243139@qq.com>
2026-04-22 04:30:11 +0800

    feat: 使用0.12新特性

--- lua/plugins/notify.lua
+++
@@ -1,3 +0,0 @@
-require("notify").setup({
-  background_colour = "#000001", -- 设置背景颜色
-})
--- lua/plugins/smear-cursor.lua
+++
@@ -1,7 +0,0 @@
-require('smear_cursor').setup({
-	cursor_color = "#fff3b0",
-	stiffness = 0.3,
-	trailing_stiffness = 0.1,
-	trailing_exponent = 5,
-	gamma = 1, 
-})
